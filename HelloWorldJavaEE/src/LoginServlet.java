c6df160904310b518b2ba34c6471fa3e1945c696
DrBingoo <awbingxian@gmail.com>
2022-01-29 15:35:56 +0800

    Login feature completed with email and password authentication.

--- HelloWorldJavaEE/src/LoginServlet.java
+++ HelloWorldJavaEE/src/LoginServlet.java
@@ -2,6 +2,11 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,6 +20,10 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private String jdbcURL = "jdbc:mysql://localhost:4306/userdetails";
+	private String jdbcUsername = "root";
+	private String jdbcPassword = "password";
+	private static final String SELECT_USER_BY_EMAIL = "select name,password,email from UserDetails where email =?";
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -27,6 +36,19 @@ public class LoginServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    protected Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
@@ -38,10 +60,31 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
-		PrintWriter writer = response.getWriter();
-		writer.println("<h1> Email " + email + "</h1>");
-		writer.println("<h1> Password " + password + "</h1>");
-		writer.close();
+		User user = null;
+		try(Connection connection = getConnection();
+				PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_EMAIL);){
+			preparedStatement.setString(1, email);
+			ResultSet rs = preparedStatement.executeQuery();
+			while (rs.next()) {
+				String name = rs.getString("name");
+				String correctPassword = rs.getString("password");
+				if(password.equals(correctPassword)) {
+					response.sendRedirect("/HelloWorldJavaEE/UserServlet/dashboard");
+				}else {
+					PrintWriter writer = response.getWriter();
+					writer.println("<h1> Either email or password is incorrect</h1>");
+					writer.close();
+				}
+				user = new User(name, password, email, "");
+			}
+			if(!rs.next()) {
+				PrintWriter writer = response.getWriter();
+				writer.println("<h1> User does not exist</h1>");
+				writer.close();
+			}
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 		doGet(request, response);
 	}
 
